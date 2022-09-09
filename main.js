cc007fdfcd6b60a28c2baa96ee9d8f0bfdc586bf
Zach Estus <zach.estus@gmail.com>
2022-09-09 14:00:48 +0000

    Creating the arrays:   main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
+
+
+let insertX = ['Willy the Goblin', 'Dwayne THE ROCK Johnson', 'Bob the Builder', 'Saint Nick'];
+let insertY = ['Antartica', 'the Holy Grail', 'Lincoln, Nebraska', 'the Void'];
+let insertZ = ['attacked by a white rabbit', 'spontaneously combusted', 'ALAN PLEASE FILL IN LATER', 'smelled what was cooking', 'turned into a slug and crawled away'];
+
+let storyText = 'It was 94 fahrenheit outside, so :insertX: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertZ:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
