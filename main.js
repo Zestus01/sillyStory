dd601b71369419d896e6654d72c84da43ea12398
Zach Estus <zach.estus@gmail.com>
2022-09-09 14:04:11 +0000

    Copied over the third number:   main.js

--- main.js
+++ main.js
@@ -8,8 +8,27 @@ function randomValueFromArray(array){
 }
 
 
-let insertX = ['Willy the Goblin', 'Dwayne THE ROCK Johnson', 'Bob the Builder', 'Saint Nick'];
+let insertX = ['Willy the Goblin', 'Dwayne "THE ROCK" Johnson', 'Bob the Builder', 'Saint Nick'];
 let insertY = ['Antartica', 'the Holy Grail', 'Lincoln, Nebraska', 'the Void'];
 let insertZ = ['attacked by a white rabbit', 'spontaneously combusted', 'ALAN PLEASE FILL IN LATER', 'smelled what was cooking', 'turned into a slug and crawled away'];
 
 let storyText = 'It was 94 fahrenheit outside, so :insertX: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertZ:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+
+randomize.addEventListener('click', result);
+
+function result() {
+
+  if(customName.value !== '') {
+    const name = customName.value;
+
+  }
+
+  if(document.getElementById("uk").checked) {
+    const weight = Math.round(300);
+    const temperature =  Math.round(94);
+
+  }
+
+  story.textContent = ;
+  story.style.visibility = 'visible';
+}
