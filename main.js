9c19f5865c1f36331f5fc88de4b335907fffd40d
Zach Estus <zach.estus@gmail.com>
2022-09-09 14:35:24 +0000

    Filled in uk conversions:   main.js

--- main.js
+++ main.js
@@ -1,34 +1,58 @@
-const customName = document.getElementById('customname');
-const randomize = document.querySelector('.randomize');
-const story = document.querySelector('.story');
+const customName = document.getElementById("customname");
+const randomize = document.querySelector(".randomize");
+const story = document.querySelector(".story");
 
-function randomValueFromArray(array){
-  const random = Math.floor(Math.random()*array.length);
+function randomValueFromArray(array) {
+  const random = Math.floor(Math.random() * array.length);
   return array[random];
 }
 
+let insertX = [
+    'Willy the Goblin',
+    'Dwayne "THE ROCK" Johnson',
+    'Bob the Builder',
+    'Saint Nick'
+];
+let insertY = [
+    'Antartica', 
+    'the Holy Grail', 
+    'Lincoln',
+    'Nebraska',
+    'the Void'
+];
+let insertZ = [
+    'attacked by a white rabbit',
+    'spontaneously combusted',
+    'ALAN PLEASE FILL IN LATER',
+    'smelled what was cooking',
+    'turned into a slug and crawled away'
+];
 
-let insertX = ['Willy the Goblin', 'Dwayne "THE ROCK" Johnson', 'Bob the Builder', 'Saint Nick'];
-let insertY = ['Antartica', 'the Holy Grail', 'Lincoln, Nebraska', 'the Void'];
-let insertZ = ['attacked by a white rabbit', 'spontaneously combusted', 'ALAN PLEASE FILL IN LATER', 'smelled what was cooking', 'turned into a slug and crawled away'];
+let storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertZ:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 
-let storyText = 'It was 94 fahrenheit outside, so :insertX: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertZ:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
-
-randomize.addEventListener('click', result);
+randomize.addEventListener("click", result);
 
 function result() {
+    let newStory = storyText;
+    if (customName.value !== "") {
+        const name = customName.value;
+        newStory = newStory.replace('Bob', customName);
+    }
 
-  if(customName.value !== '') {
-    const name = customName.value;
+    if (document.getElementById("uk").checked) {
+        const weight = (Math.round(300) * 0.0714286) + ' stones';
+        const temperature = (Math.round(94) - 32) * (5/9) + ' centigrade';
+        newStory = newStory.replace('94 fahrenheit', temperature);
+        newStory = newStory.replace('300 pounds', weight);
+    }
+    let xItem = randomValueFromArray(insertX);
+    let yItem = randomValueFromArray(insertY);
+    let zItem = randomValueFromArray(insertZ);
 
-  }
+    newStory = newStory.replace(":insertx:", xItem);
+    newStory = newStory.replace(":inserty:", yItem);
+    newStory = newStory.replace(":insertz:", zItem);
 
-  if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
-
-  }
-
-  story.textContent = ;
-  story.style.visibility = 'visible';
+    story.textContent = newStory;
+    story.style.visibility = "visible";
 }
